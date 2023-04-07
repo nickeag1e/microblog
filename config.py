59cb7b1c9a60b93b058b1ae515b74845c9513cfc
nickeag1e <125014876+nickeag1e@users.noreply.github.com>
2023-04-07 16:52:58 +0300

    made some changes

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'MySuperSecretPassword123190'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')

5383eac84ef78c60ec0e99c12107a12115145604
nickeag1e <125014876+nickeag1e@users.noreply.github.com>
2023-04-07 17:19:18 +0300

    made some changes

--- app/models.py
+++ app/models.py
@@ -7,5 +7,4 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return f'<' \
-               f'User {self.username}>'
+        return f'<User {self.username}>'

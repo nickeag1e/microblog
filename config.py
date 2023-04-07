e871df711cb62cb57f739c5436bf162fae82c2b6
nickeag1e <125014876+nickeag1e@users.noreply.github.com>
2023-04-07 16:54:10 +0300

    config

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'MySuperSecretPassword123190'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

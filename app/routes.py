2ae7597c39f4c8ed6e6bbe0d829127cecb10e5aa
nickeag1e <125014876+nickeag1e@users.noreply.github.com>
2023-05-12 17:02:00 +0300

    made some changes

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,12 @@ from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from app.models import User
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Nickolay'}
     posts = [
@@ -33,7 +34,7 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
@@ -41,3 +42,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='LogIn', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
